ea2dca2ec2d2d913057a16840fc40efea69d0240
Fernando Lizano <167820462+GuardianDeity@users.noreply.github.com>
2026-06-27 02:02:31 -0500

    feat: prepare for display

--- src/main/java/com/upc/creditovehicular/config/corsConfig.java
+++ src/main/java/com/upc/creditovehicular/config/corsConfig.java
@@ -1,5 +1,6 @@
 package com.upc.creditovehicular.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -7,17 +8,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class corsConfig {
+
+    @Value("${ip.frontend:http://localhost:4200}")
+    private String frontendUrl;
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins(frontendUrl)
                         .allowedMethods("*")
                         .allowedHeaders("*");
-            };
+            }
         };
     }
-
 }
